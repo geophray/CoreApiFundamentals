93d0995137da5be22a50d9c57de3c161530569bd
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-21 09:32:18 -0600

    Add LinkGenerator for new resource URI

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -3,10 +3,12 @@ using CoreCodeCamp.Data;
 using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore.Update;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,11 +20,13 @@ namespace CoreCodeCamp.Controllers
     {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper mapper)
+        public CampsController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
         {
             _repository = repository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
 
         [HttpGet]
@@ -36,7 +40,7 @@ namespace CoreCodeCamp.Controllers
             }
             catch (Exception)
             {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure in 'GetAllCampsAsync'");
             }
 
         }
@@ -54,7 +58,7 @@ namespace CoreCodeCamp.Controllers
             }
             catch (Exception)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure in 'GetCampAsync'");
             }
         }
 
@@ -71,7 +75,7 @@ namespace CoreCodeCamp.Controllers
             }
             catch (Exception)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure in 'GetAllCampsByEventDate'");
             }
         }
 
@@ -79,17 +83,24 @@ namespace CoreCodeCamp.Controllers
         {
             try
             {
+                // Generate link for new resource
+                var location = _linkGenerator.GetPathByAction("Get", "Camps", new { moniker = model.Moniker });
+                if (string.IsNullOrWhiteSpace(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+
                 // Create a new Camp
                 var camp = _mapper.Map<Camp>(model);
                 _repository.Add(camp);
                 if (await _repository.SaveChangesAsync())
                 {
-                    return Created("", _mapper.Map<CampModel>(camp));
+                    return Created(location, _mapper.Map<CampModel>(camp));
                 }
             }
             catch (Exception)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure in 'SaveChangesAsync'");
             }
             return BadRequest();
         }
