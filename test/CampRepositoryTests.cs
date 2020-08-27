02fe965be2ff6a134b56a820519997c55dbf0107
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-27 09:42:55 -0600

    Stub out more tests for camp getting methods

--- test/CampRepositoryTests.cs
+++ test/CampRepositoryTests.cs
@@ -9,28 +9,83 @@ namespace CoreCodeCamp.Test
 {
     public class CampRepositoryTests
     {
-
-        // arrange
-        // act
-        // assert 
+        // GetAllCampsByEventDate Method Tests =========================== 
+        [Fact]
+        public void GetAllCampsByEventDate_ReturnsArrayOfCamps()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert 
+        }
 
         [Fact]
-        public void GetCampsAsync_ReturnsArrayOfCamps()
+        public void GetAllCampsByEventDate_InDescendingOrder()
         {
+            throw new NotImplementedException();
             // arrange
-                // create fake repository
+            // act
+            // assert 
+        }
+
+        [Fact]
+        public void GetAllCampsByEventDate_TalksIncludedIf_IncludeTalksTrue()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert 
+        }
+
+        [Fact]
+        public void GetAllCampsByEventDate_SpeakersIncludedIf_TalksIncluded()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert 
+        }
+
+        [Fact]
+        public void GetAllCampsByEventDate_TalksNotIncludedIf_IncludeTalksFalse()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert 
+        }
+
+
+        // GetAllCampsAsync Method Tests =========================== 
+
+        [Fact]
+        public void GetAllCampsAsync_ReturnsArrayOfCamps()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // create fake repository
 
             // act
-                // Get camps from repository
+            // Get camps from repository
 
             // assert 
-                // Verify that it returns an array of objects.
+            // Verify that it returns an array of objects.
 
         }
 
         [Fact]
-        public void TalksIncludedIf_IncludeTalksTrue()
+        public void GetAllCampsAsync_InDescendingOrder()
         {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert 
+        }
+
+        [Fact]
+        public void GetAllCampsAsync_TalksIncludedIf_IncludeTalksTrue()
+        {
+            throw new NotImplementedException();
             // arrange
             // act
             // assert
@@ -38,8 +93,72 @@ namespace CoreCodeCamp.Test
         }
 
         [Fact]
-        public void TalksNotIncludedIf_IncludeTalksFalse()
+        public void GetAllCampsAsync_SpeakersIncludedIf_TalksIncluded()
         {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert 
+        }
+
+        [Fact]
+        public void GetAllCampsAsync_TalksNotIncludedIf_IncludeTalksFalse()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert
+        }
+
+        // GetCampAsync Method Tests =========================== 
+
+        [Fact]
+        public void GetCampAsync_ReturnsSingleCampObject()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // create fake repository
+
+            // act
+            // Get camps from repository
+
+            // assert 
+            // Verify that it returns an array of objects.
+
+        }
+
+        [Fact]
+        public void GetCampAsync_MonikerMatchesReturnedCamp()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert 
+        }
+
+        [Fact]
+        public void GetCampAsync_TalksIncludedIf_IncludeTalksTrue()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert
+
+        }
+
+        [Fact]
+        public void GetCampAsync_SpeakersIncludedIf_TalksIncluded()
+        {
+            throw new NotImplementedException();
+            // arrange
+            // act
+            // assert 
+        }
+
+        [Fact]
+        public void GetCampAsync_TalksNotIncludedIf_IncludeTalksFalse()
+        {
+            throw new NotImplementedException();
             // arrange
             // act
             // assert
